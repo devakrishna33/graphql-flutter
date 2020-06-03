273fb70392f817d05c345cd26aa4d7a2eb0174a2
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2020-06-04 01:30:47 +0530

    Updated query_manager

--- packages/graphql/lib/src/core/query_manager.dart
+++ packages/graphql/lib/src/core/query_manager.dart
@@ -238,7 +238,7 @@ class QueryManager {
     bool writeToCache = false,
   }) {
     final ObservableQuery observableQuery = getQuery(queryId);
-    if (writeToCache) {
+    if (writeToCache && observableQuery != null) {
       cache.write(
         observableQuery.options.toKey(),
         queryResult.data,
